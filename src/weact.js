2dc366dd72f26a8dab50a97c79445f709007ed9a
Zloy Air <zloyair@olevchenko-mb.netis>
2021-07-25 22:21:22 +0300

    add babel

--- src/weact.js
+++ src/weact.js
@@ -1,27 +1,38 @@
 const render = (element, container) => {
-  const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(element.type);
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  const isProperty = key => key !== "children";
   Object.keys(element.props)
+    .filter(isProperty)
     .forEach(name => {
       dom[name] = element.props[name];
     });
-  element.children.forEach(child => render(child, dom));
+  element.props.children.forEach(child => render(child, dom));
   container.appendChild(dom);
 }
 
-const createTextElement = text => ({
-  type: "TEXT_ELEMENT",
-  props: {
-    nodeValue: text,
-  },
-  children: []
-})
+const createTextElement = (text) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
 
-const createElement = (type, props = {}, ...children) => {
+const createElement = (type, props, ...children) => {
   return {
     type,
-    props,
-    children: children.map(element => typeof element === "object" ? element : createTextElement(element))
-  }
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
 }
 
 export const Weact = {
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: './dist',
-        index: 'index.html',
-    },
-    plugins: [new HtmlWebpackPlugin({
-        inject: "body",
-        template: './dist/index.html'
-    })],
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    index: 'index.html',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: "body",
+    template: './dist/index.html'
+  })],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ]
+  },
 };
