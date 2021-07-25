5d007f178f62e4629446f01dae1bd0c34375fa1c
Zloy Air <zloyair@olevchenko-mb.netis>
2021-07-25 15:30:55 +0300

    create render function

--- src/weact.js
+++ src/weact.js
@@ -1,12 +1,30 @@
-const render = () => {
-
+const render = (element, container) => {
+  const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(element.type);
+  Object.keys(element.props)
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+  element.children.forEach(child => render(child, dom));
+  container.appendChild(dom);
 }
 
-const createElement = (type) => {
+const createTextElement = text => ({
+  type: "TEXT_ELEMENT",
+  props: {
+    nodeValue: text,
+  },
+  children: []
+})
 
+const createElement = (type, props = {}, ...children) => {
+  return {
+    type,
+    props,
+    children: children.map(element => typeof element === "object" ? element : createTextElement(element))
+  }
 }
 
 export const Weact = {
-    render,
-    createElement,
+  render,
+  createElement,
 }
